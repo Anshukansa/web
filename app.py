0e46c220e1c7e5a7d3cab478978225d5b78cdf46
Anshukansa <91750490+Anshukansa@users.noreply.github.com>
2025-04-09 14:46:39 +1000

    Updated deployment configuration

--- app.py
+++ app.py
@@ -42,7 +42,8 @@ def create_app(config_class=config.Config):
     
     return app
 
+app = create_app()  # Create the app at the module level
+
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
